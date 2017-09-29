a26e309d9132314631094c24c2135f9640cd5d38
Anders Ekdahl <anders@Anderss-MacBook-Pro.local>
2017-09-29 12:37:32 +0200

    added functionality for temperature

--- src/app/temperature/temperature.component.ts
+++ src/app/temperature/temperature.component.ts
@@ -7,9 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemperatureComponent implements OnInit {
 
+  celsius: number;
+  fahrenheit: number;
+  kelvin: number;
+  
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  calc(event){
+    let callingField = event.target.id;
+    if (callingField == 'celsius') {
+      this.fahrenheit = this.calcFahrenheitFromCelsius();
+      this.kelvin = this.calcKelvinFromCelsius();
+    }
+    if (callingField == 'fahrenheit') {
+      this.celsius = this.calcCelsiusFromFahrenheit();
+      this.kelvin = this.calcKelvinFromFahrenheit();
+    }
+    if (callingField == 'kelvin') {
+      this.celsius = this.calcCelsiusFromKelvin();
+      this.fahrenheit = this.calcFahrenheitFromKelvin();
+    }
   }
 
+  clearAll(){
+    this.celsius = null;
+    this.fahrenheit = null;
+    this.kelvin = null;
+  }
+
+  calcFahrenheitFromCelsius(){
+    if (this.celsius != null) {
+     return (this.celsius * 9/5) + 32;
+    } else {return null;}
+  }
+  calcKelvinFromCelsius(){
+    return this.celsius - 273.15;    
+  }
+  calcCelsiusFromFahrenheit(){
+    return (this.fahrenheit - 32) * 5/9;
+  }
+  calcKelvinFromFahrenheit(){
+    return (this.fahrenheit + 459.67) / 1.8;
+  }
+  calcCelsiusFromKelvin(){
+    return this.kelvin + 273.15;
+  }
+  calcFahrenheitFromKelvin(){
+    return (this.kelvin * 9/5) - 459.67;
+  }
 }
