d2e816b3e9b97eef0e81a23c20777008531f4479
ekan <anders.ekdahl@cag.se>
2017-10-16 17:03:48 +0200

    weigth conversion tests implemented

--- e2e/testdata/weigthdata.ts
+++ e2e/testdata/weigthdata.ts
@@ -25,7 +25,61 @@ export class WeigthData {
                 "kilo": "314",
                 "pound": "692.2515",
                 "stone": "49.4465"                
+            }
+        ],
+        "poundtests": [
+            {
+                "kilo": "0.4536",
+                "pound": "1",
+                "stone": "0.0714"
             },
+            {
+                "kilo": "0",
+                "pound": "0",
+                "stone": "0"                
+            },
+            {
+                "kilo": "2",
+                "pound": "4.4092",
+                "stone": "0.3149"                
+            },
+            {
+                "kilo": "6.3503",
+                "pound": "14",
+                "stone": "1"                
+            },
+            {
+                "kilo": "680.3886",
+                "pound": "1500",
+                "stone": "107.1429"                
+            }
+        ],
+        "stonetests": [
+            {
+                "kilo": "19.0509",
+                "pound": "42",
+                "stone": "3"
+            },
+            {
+                "kilo": "0",
+                "pound": "0",
+                "stone": "0"                
+            },
+            {
+                "kilo": "5",
+                "pound": "11.023",
+                "stone": "0.787358"                
+            },
+            {
+                "kilo": "781.0861",
+                "pound": "1722",
+                "stone": "123"                
+            },
+            {
+                "kilo": "14891.4375",
+                "pound": "32830",
+                "stone": "2345"                
+            }
         ]
-    }
+     }
 }
--- e2e/tests/weigth.e2e-spec.ts
+++ e2e/tests/weigth.e2e-spec.ts
@@ -30,20 +30,54 @@ describe('Check functionality of weigth view', function () {
             expect(weigthPage.isRadiobtnChecked(weigthElementIds.four_decimals)).toBeTruthy();
             browser.sleep(browserSleep);
         });
-        it('Testing conversion from kilo', () => {
+        it('Testing conversion from kilos', () => {
             weigthPage.setValueOfTxtField(weigthElementIds.kilograms, '1');
             let kiloTests = valData.weigthTestData.kilotests;
             for (let i in kiloTests) {
                 let currentKilo = kiloTests[i].kilo;
                 let currentPound = kiloTests[i].pound;
                 let currentStone = kiloTests[i].stone;
-                let kiloErrMsg: string = 'kilo was not correctly given for: ' + currentKilo + ' kilo';
-                let poundErrMsg: string = 'Pound conversion failed for: ' + currentKilo + ' kilo';
-                let stoneErrMsg: string = 'Stone conversion failed for: ' + currentKilo + ' kilo';
+                let kiloErrMsg: string = 'Kilo was not correctly given for: ' + currentKilo + ' kilos';
+                let poundErrMsg: string = 'Pound conversion failed for: ' + currentKilo + ' kilos';
+                let stoneErrMsg: string = 'Stone conversion failed for: ' + currentKilo + ' kilos';
                 weigthPage.emptyAndSetTextField(weigthElementIds.kilograms, currentKilo);
                 expect(weigthPage.getValueOfTxtField(weigthElementIds.kilograms)).toBe(currentKilo).then(()=>{console.log('Testing with: ' + currentKilo + ' kilo')});;
                 expect(weigthPage.getValueOfTxtField(weigthElementIds.pounds)).toBe(currentPound, poundErrMsg);
                 expect(weigthPage.getValueOfTxtField(weigthElementIds.stones)).toBe(currentStone, stoneErrMsg);
+            }
+            browser.sleep(browserSleep);
+        });
+        it('Testing conversion from Pounds', () => {
+            weigthPage.setValueOfTxtField(weigthElementIds.kilograms, '1');
+            let poundTests = valData.weigthTestData.poundtests;
+            for (let i in poundTests) {
+                let currentKilo = poundTests[i].kilo;
+                let currentPound = poundTests[i].pound;
+                let currentStone = poundTests[i].stone;
+                let kiloErrMsg: string = 'Kilo conversion failed for: ' + currentPound + ' pounds';
+                let poundErrMsg: string = 'Pound was not correctly given for: ' + currentPound + ' pounds';
+                let stoneErrMsg: string = 'Stone conversion failed for: ' + currentPound + ' pounds';
+                weigthPage.emptyAndSetTextField(weigthElementIds.pounds, currentPound);
+                expect(weigthPage.getValueOfTxtField(weigthElementIds.kilograms)).toBe(currentKilo);
+                expect(weigthPage.getValueOfTxtField(weigthElementIds.pounds)).toBe(currentPound, poundErrMsg).then(()=>{console.log('Testing with: ' + currentPound + ' pounds')});
+                expect(weigthPage.getValueOfTxtField(weigthElementIds.stones)).toBe(currentStone, stoneErrMsg);
+            }
+            browser.sleep(browserSleep);
+        });
+        it('Testing conversion from Stones', () => {
+            weigthPage.setValueOfTxtField(weigthElementIds.kilograms, '1');
+            let stoneTests = valData.weigthTestData.stonetests;
+            for (let i in stoneTests) {
+                let currentKilo = stoneTests[i].kilo;
+                let currentPound = stoneTests[i].pound;
+                let currentStone = stoneTests[i].stone;
+                let kiloErrMsg: string = 'Kilo conversion failed for: : ' + currentStone + ' stones';
+                let poundErrMsg: string = 'Pound conversion failed for: ' + currentStone + ' stones';
+                let stoneErrMsg: string = 'Stone was not correctly given for: : ' + currentStone + ' stones';
+                weigthPage.emptyAndSetTextField(weigthElementIds.stones, currentStone);
+                expect(weigthPage.getValueOfTxtField(weigthElementIds.kilograms)).toBe(currentKilo);;
+                expect(weigthPage.getValueOfTxtField(weigthElementIds.pounds)).toBe(currentPound, poundErrMsg);
+                expect(weigthPage.getValueOfTxtField(weigthElementIds.stones)).toBe(currentStone, stoneErrMsg).then(()=>{console.log('Testing with: ' + currentStone + ' stones')});
             }
             browser.sleep(browserSleep);
         });
